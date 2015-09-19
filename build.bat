fb4f24571089276517d761f74adb20b28966ea6d
Jason Mitchell <jasonmitchell@live.co.uk>
2015-09-19 21:55:18 +0100

    Updating nuget package build

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 nuget pack ./src/Sequin.Core/Sequin.Core.csproj -IncludeReferencedProjects -Prop Configuration=Release
 nuget pack ./src/Sequin/Sequin.csproj -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack ./src/Sequin.Autofac/Sequin.Autofac.csproj -IncludeReferencedProjects -Prop Configuration=Release
