8ce6be075c689e3d09831b5fe82aa60b757422a6
Jason Mitchell <jasonmitchell@live.co.uk>
2015-09-20 20:31:38 +0100

    Adding integration test project

---
+++ tests/Sequin.Integration/CommandIssuingFacts.cs
@@ -0,0 +1,29 @@
+﻿namespace Sequin.Integration
+{
+    using System.Net;
+    using Core;
+    using Xunit;
+    using Xunit.Should;
+
+    public class WhenCommandHandlerExists : SequinSpecification
+    {
+        [Fact]
+        public async void ReturnsOkStatusCode()
+        {
+            var response = await IssueCommand("TestCommand");
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
+
+        private class TestCommandHandler : IHandler<TestCommand>
+        {
+            public void Handle(TestCommand command)
+            {
+                // Do nothing
+            }
+        }
+
+        private class TestCommand
+        {
+        }
+    }
+}
---
+++ tests/Sequin.Integration/SequinSpecification.cs
@@ -0,0 +1,26 @@
+﻿namespace Sequin.Integration
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Microsoft.Owin.Testing;
+
+    public abstract class SequinSpecification
+    {
+        protected SequinSpecification()
+        {
+            Server = TestServer.Create(app =>
+            {
+                app.UseSequin();
+            });
+        }
+
+        protected TestServer Server { get; }
+
+        protected async Task<HttpResponseMessage> IssueCommand(string commandName)
+        {
+            return await Server.CreateRequest("/commands")
+                               .AddHeader("command", commandName)
+                               .SendAsync("PUT");
+        }
+    }
+}
