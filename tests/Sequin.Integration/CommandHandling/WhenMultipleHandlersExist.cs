87e1f08b394ffc3fb7cb4c88a6cea4de44ad21f3
Jason Mitchell <jasonmitchell@live.co.uk>
2015-09-26 09:42:34 +0100

    More tests

--- tests/Sequin.Integration/CommandHandling/WhenMultipleHandlersExist.cs
+++ tests/Sequin.Integration/CommandHandling/WhenMultipleHandlersExist.cs
@@ -1,19 +1,48 @@
 ﻿namespace Sequin.Integration.CommandHandling
 {
+    using System.Net;
+    using Core;
+    using Newtonsoft.Json.Linq;
+    using Should;
     using Xunit;
 
-    public class WhenMultipleHandlersExist
+    public class WhenMultipleHandlersExist : SequinSpecification
     {
-        [Fact(Skip = "Not Implemented")]
+        [Fact]
         public void ReturnsInternalServerError()
         {
-            
+            var response = IssueCommand(new MultiHandlerCommand());
+            response.StatusCode.ShouldEqual(HttpStatusCode.InternalServerError);
         }
 
-        [Fact(Skip = "Not Implemented")]
-        public void ReturnsExceptionInformation()
+        [Fact]
+        public async void ReturnsExceptionInformation()
         {
-            
+            var response = IssueCommand(new MultiHandlerCommand());
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            dynamic json = JToken.Parse(responseBody);
+            string message = json.message;
+
+            message.ShouldEqual("Multiple handlers exist for the given command type.");
         }
+
+        private class MultiHandlerCommandHandler1 : IHandler<MultiHandlerCommand>
+        {
+            public void Handle(MultiHandlerCommand command)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        private class MultiHandlerCommandHandler2 : IHandler<MultiHandlerCommand>
+        {
+            public void Handle(MultiHandlerCommand command)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        private class MultiHandlerCommand { }
     }
 }
