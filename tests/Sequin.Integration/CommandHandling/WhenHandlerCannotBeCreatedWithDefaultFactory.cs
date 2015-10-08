7116b022d0399219989a6f4d4ef0b6684cfe4779
Jason Mitchell <jasonmitchell@live.co.uk>
2015-10-08 21:24:04 +0100

    Additional tests

---
+++ tests/Sequin.Integration/CommandHandling/WhenHandlerCannotBeCreatedWithDefaultFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace Sequin.Integration.CommandHandling
+{
+    using System.Net;
+    using Fakes;
+    using Newtonsoft.Json.Linq;
+    using Should;
+    using Xunit;
+
+    public class WhenHandlerCannotBeCreatedWithDefaultFactory : SequinSpecification
+    {
+        [Fact]
+        public void ReturnsInternalServerError()
+        {
+            var response = IssueCommand(new UnconstructableCommandHandlerTest());
+            response.StatusCode.ShouldEqual(HttpStatusCode.InternalServerError);
+        }
+
+        [Fact]
+        public async void ReturnsExceptionInformation()
+        {
+            var response = IssueCommand(new UnconstructableCommandHandlerTest());
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            dynamic json = JToken.Parse(responseBody);
+            string message = json.message;
+
+            message.ShouldEqual("No parameterless constructor defined for this object.");
+        }
+    }
+}
---
+++ tests/Sequin.Integration/Fakes/UnconstructableCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Sequin.Integration.Fakes
+{
+    using Core;
+
+    public class UnconstructableCommandHandler : IHandler<UnconstructableCommandHandlerTest>
+    {
+        public UnconstructableCommandHandler(object obj)
+        {
+            // No parameterless constructor to make handler unconstructable with default factory
+        }
+
+        public void Handle(UnconstructableCommandHandlerTest command)
+        {
+            // Do nothing
+        }
+    }
+
+    public class UnconstructableCommandHandlerTest { }
+}
