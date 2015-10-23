c857fac3b60ae5652107be28c214e3080ae0c780
Jason Mitchell <jasonmitchell@live.co.uk>
2015-10-23 21:34:56 +0100

    Adding Sequin.ClaimsAuthentication

---
+++ src/Sequin.ClaimsAuthentication/AuthorizeCommandAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace Sequin.ClaimsAuthentication
+{
+    using System;
+    using System.Linq;
+    using System.Security.Claims;
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    public class AuthorizeCommandAttribute : Attribute
+    {
+        private readonly string[] requiredRoles;
+
+        public AuthorizeCommandAttribute(params string[] requiredRoles)
+        {
+            this.requiredRoles = requiredRoles;
+        }
+
+        public void Authorize(CommandAuthorizationContext context)
+        {
+            if (requiredRoles.Any(role => !context.Identity.HasClaim(ClaimTypes.Role, role)))
+            {
+                context.Reject();
+            }
+        }
+    }
+}
---
+++ src/Sequin.ClaimsAuthentication/CommandAuthorizationContext.cs
@@ -0,0 +1,23 @@
+﻿namespace Sequin.ClaimsAuthentication
+{
+    using System;
+    using System.Security.Claims;
+
+    public class CommandAuthorizationContext
+    {
+        public CommandAuthorizationContext(ClaimsIdentity identity, Type commandType)
+        {
+            Identity = identity;
+            CommandType = commandType;
+        }
+
+        public void Reject()
+        {
+            IsAuthorized = false;
+        }
+
+        public bool IsAuthorized { get; private set; } = true;
+        public ClaimsIdentity Identity { get; }
+        public Type CommandType { get; }
+    }
+}
