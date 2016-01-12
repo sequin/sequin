99b355c5eeeda6fc900566389b81cda4ab414464
Jason Mitchell <jasonmitchell@live.co.uk>
2016-01-12 13:52:14 +0000

    Removing await call from dynamically invoked mthd
    Fixes #23

--- src/Sequin.FluentValidation/Middleware/ValidateCommand.cs
+++ src/Sequin.FluentValidation/Middleware/ValidateCommand.cs
@@ -8,6 +8,7 @@
     using System.Threading.Tasks;
     using Extensions;
     using global::FluentValidation;
+    using global::FluentValidation.Results;
     using Microsoft.Owin;
 
     public class ValidateCommand : OwinMiddleware
@@ -22,47 +23,60 @@
         public override async Task Invoke(IOwinContext context)
         {
             var command = context.GetCommand();
+            var validationResult = Validate(command);
+
+            if (validationResult != null && validationResult.IsValid)
+            {
+                await Next.Invoke(context);
+            }
+            else if (validationResult != null)
+            {
+                WriteErrorResponse(context, validationResult);
+            }
+            else
+            {
+                context.Response.BadRequest("No command validator registered for the specified command type.");
+            }
+        }
+
+        private static void WriteErrorResponse(IOwinContext context, ValidationResult validationResult)
+        {
+            var errors = validationResult.Errors
+                                         .GroupBy(k => k.PropertyName)
+                                         .ToDictionary(g => g.Key, g => g.Select(x => x.ErrorMessage));
+
+            context.Response.BadRequest("The command contained validation errors.");
+            context.Response.Json(errors);
+        }
+
+        private ValidationResult Validate(object command)
+        {
             var commandType = command.GetType();
 
             try
             {
-                Expression<Action<object>> expression = x => Validate(context, x);
-                var methodCallExpression = (MethodCallExpression) expression.Body;
+                Expression<Action<object>> expression = x => ExecuteValidator(x);
+                var methodCallExpression = (MethodCallExpression)expression.Body;
                 var methodInfo = methodCallExpression.Method.GetGenericMethodDefinition().MakeGenericMethod(commandType);
 
-                await Task.Run(() => methodInfo.Invoke(this, new[] {context, command}));
+                var result = (ValidationResult)methodInfo.Invoke(this, new[] { command });
+                return result;
 
             }
             catch (TargetInvocationException ex)
             {
                 ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
             }
+
+            return null;
         }
 
-        private async Task Validate<T>(IOwinContext context, T command)
+        private ValidationResult ExecuteValidator<T>(T command)
         {
             var validator = validatorFactory.GetValidator<T>();
+            var result = validator?.Validate(command);
 
-            if (validator == null)
-            { 
-                context.Response.BadRequest("No command validator registered for the specified command type.");
-                return;
-            }
-
-            var result = validator.Validate(command);
-            if (!result.IsValid)
-            {
-                var errors = result.Errors
-                    .GroupBy(k => k.PropertyName)
-                    .ToDictionary(g => g.Key, g => g.Select(x => x.ErrorMessage));
-
-                context.Response.BadRequest("The command contained validation errors.");
-                context.Response.Json(errors);
-            }
-            else
-            {
-                await Next.Invoke(context);
-            }
+            return result;
         }
     }
 }
