370fbdffac00fdbc1b278b9cabc3e4ac3a81f01b
Jason Mitchell <jasonmitchell@live.co.uk>
2016-01-15 12:18:44 +0000

    Replacing existing tests with XBehave

---
+++ tests/Sequin.Integration/FeatureBase.cs
@@ -0,0 +1,20 @@
+﻿namespace Sequin.Integration
+{
+    using Microsoft.Owin.Testing;
+    using Xbehave;
+
+    public abstract class FeatureBase
+    {
+        protected SequinOptions Options { get; set; }
+        protected TestServer Server { get; private set; }
+
+        [Background]
+        public void Background()
+        {
+            Server = TestServer.Create(app =>
+                                       {
+                                           app.UseSequin(Options ?? new SequinOptions());
+                                       });
+        }
+    }
+}
