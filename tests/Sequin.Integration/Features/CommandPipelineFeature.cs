73150340e43405260822f4960ee436e9c2ecd81d
Jason Mitchell <jasonmitchell@live.co.uk>
2016-01-15 12:47:48 +0000

    Adding tests for command pipeline

---
+++ tests/Sequin.Integration/Features/CommandPipelineFeature.cs
@@ -0,0 +1,126 @@
+﻿namespace Sequin.Integration.Features
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Core;
+    using Extensions;
+    using Fakes;
+    using FluentAssertions;
+    using Infrastructure;
+    using Microsoft.Owin;
+    using Sequin.Extensions;
+    using Xbehave;
+
+    public class CommandPipelineFeature : FeatureBase
+    {
+        [Background]
+        public void FeatureBackground()
+        {
+            Options = new SequinOptions
+                      {
+                          CommandPipeline = new []
+                                            {
+                                                new CommandPipelineStage(typeof(Passthrough)),
+                                                new CommandPipelineStage(typeof(ConditionalCapture))
+                                            }
+                      };
+        }
+
+        [Scenario]
+        public void PassthroughPipelineAndIssue(string commandName, string command, HttpResponseMessage response)
+        {
+            "Given I have a command that should passthrough the pipeline"
+                .Given(() =>
+                       {
+                           commandName = "PassthroughCommand";
+                           command = "{A:1,B:2}";
+                       });
+
+            "When I issue an HTTP request"
+                .When(() =>
+                      {
+                          response = Server.PutCommand("/commands", commandName, command);
+                      });
+
+            "Then the response should return as OK"
+                .Then(() =>
+                      {
+                          response.StatusCode.Should().Be(HttpStatusCode.OK);
+                      });
+        }
+
+        [Scenario]
+        public void StopRequestInPipeline(string commandName, string command, HttpResponseMessage response)
+        {
+            "Given I have a command that should be captured in the pipeline"
+                .Given(() =>
+                       {
+                           commandName = "TrackedCommand";
+                           command = "{A:1,B:2}";
+                       });
+
+            "When I issue an HTTP request"
+                .When(() =>
+                      {
+                          response = Server.PutCommand("/commands", commandName, command);
+                      });
+
+            "Then the command should not be issued"
+                .Then(() =>
+                      {
+                          var handledCommand = TrackedCommandHandler.LastCommand;
+                          handledCommand.Should().BeNull();
+                      });
+
+            "And the response should be configured by the pipeline"
+                .And(() =>
+                     {
+                         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+                     });
+        }
+
+        private class Passthrough : OwinMiddleware
+        {
+            public Passthrough(OwinMiddleware next) : base(next) {}
+
+            public override async Task Invoke(IOwinContext context)
+            {
+                await Next.Invoke(context);
+            }
+        }
+
+        private class ConditionalCapture : OwinMiddleware
+        {
+            public ConditionalCapture(OwinMiddleware next) : base(next) {}
+
+            public override async Task Invoke(IOwinContext context)
+            {
+                var commandName = context.GetCommand().GetType().Name;
+
+                if (commandName == "TrackedCommand")
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                }
+                else
+                {
+                    await Next.Invoke(context);
+                }
+            }
+        }
+
+        private class PassthroughCommand
+        {
+            public int A { get; set; }
+            public int B { get; set; }
+        }
+
+        private class PassthroughCommandHandler : IHandler<PassthroughCommand>
+        {
+            public void Handle(PassthroughCommand command)
+            {
+                
+            }
+        }
+    }
+}
