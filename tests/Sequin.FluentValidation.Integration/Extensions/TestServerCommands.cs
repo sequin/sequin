16e71a6f616038445c2e3bb4d24f504e6e878645
Jason Mitchell <jasonmitchell@live.co.uk>
2016-01-15 17:14:29 +0000

    Removing unused method

--- tests/Sequin.FluentValidation.Integration/Extensions/TestServerCommands.cs
+++ tests/Sequin.FluentValidation.Integration/Extensions/TestServerCommands.cs
@@ -26,25 +26,5 @@
                 throw ex.InnerException;
             }
         }
-
-        public static HttpResponseMessage PutCommand(this TestServer server, string url, string command)
-        {
-            var request = server.CreateRequest(url)
-                                .AddHeader("content-type", "application/json");
-
-            if (command != null)
-            {
-                request = request.And(x => x.Content = new StringContent(command));
-            }
-
-            try
-            {
-                return request.SendAsync("PUT").Result;
-            }
-            catch (AggregateException ex)
-            {
-                throw ex.InnerException;
-            }
-        }
     }
 }
