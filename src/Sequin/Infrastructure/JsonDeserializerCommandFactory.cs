1ad4261cb42cc927d17637c5401deec5e0b63629
Jason Mitchell <jasonmitchell@live.co.uk>
2016-03-09 13:54:04 +0000

    Adding support for custom serializer settings

--- src/Sequin/Infrastructure/JsonDeserializerCommandFactory.cs
+++ src/Sequin/Infrastructure/JsonDeserializerCommandFactory.cs
@@ -9,6 +9,15 @@
 
     public class JsonDeserializerCommandFactory : ICommandFactory
     {
+        private readonly JsonSerializerSettings serializerSettings;
+
+        public JsonDeserializerCommandFactory() : this(new JsonSerializerSettings()) { }
+
+        public JsonDeserializerCommandFactory(JsonSerializerSettings serializerSettings)
+        {
+            this.serializerSettings = serializerSettings;
+        }
+
         public object Create(Type commandType, IDictionary<string, object> environment)
         {
             var request = new OwinRequest(environment);
@@ -17,7 +26,7 @@
                 var requestBody = streamReader.ReadToEnd();
                 try
                 {
-                    var command = Convert.ChangeType(JsonConvert.DeserializeObject(requestBody, commandType), commandType);
+                    var command = Convert.ChangeType(JsonConvert.DeserializeObject(requestBody, commandType, serializerSettings), commandType);
                     return command;
                 }
                 catch (JsonReaderException ex)
---
+++ tests/Sequin.Integration/Features/JsonDeserializerCommandFactoryFeature.cs
@@ -0,0 +1,78 @@
+﻿namespace Sequin.Integration.Features
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Core;
+    using Extensions;
+    using FluentAssertions;
+    using Infrastructure;
+    using Microsoft.Owin.Testing;
+    using Newtonsoft.Json;
+    using Xbehave;
+
+    public class JsonDeserializerCommandFactoryFeature
+    {
+        private CommandTrackingPostProcessor postProcessor;
+
+        [Background]
+        public void Background()
+        {
+            postProcessor = new CommandTrackingPostProcessor();
+        }
+
+        [Scenario]
+        public void CustomisableSerializerSettings(TestServer server, string commandName, string command, HttpResponseMessage response)
+        {
+            "Given I have custom serializer settings"
+                .Given(() =>
+                {
+                    var serializerSettings = new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore};
+
+                    server = TestServer.Create(app =>
+                    {
+                        app.UseSequin(new SequinOptions
+                        {
+                            CommandFactory = new JsonDeserializerCommandFactory(serializerSettings),
+                            PostProcessor = postProcessor
+                        });
+                    });
+                });
+
+            "And I have a command which requires these settings"
+                .And(() =>
+                {
+                    commandName = "TestCommand";
+                    command = "{A:null}";
+                });
+
+            "When I issue an HTTP request"
+                .When(() =>
+                {
+                    response = server.PutCommand("/commands", commandName, command);
+                });
+
+            "Then the command should have been executed"
+                .Then(() =>
+                {
+                    postProcessor.ExecutedCommands.ContainsKey(commandName).Should().BeTrue();
+
+                    var serializedCommand = postProcessor.ExecutedCommands[commandName];
+                    serializedCommand.Should().Contain(Guid.Empty.ToString());
+                });
+        }
+
+        private class TestCommand
+        {
+            public Guid A { get; set; }
+        }
+
+        private class TestCommandHandler : IHandler<TestCommand>
+        {
+            public Task Handle(TestCommand command)
+            {
+                return Task.FromResult(0);
+            }
+        }
+    }
+}
