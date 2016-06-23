8c2d6d31c7ae3211bceba8e4634877eb4b378f13
Jason Mitchell <jasonmitchell@live.co.uk>
2016-06-23 10:17:17 +0100

    Converting sample to self-hosted

---
+++ src/Sequin.Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Sequin.Sample
+{
+    using System;
+    using Microsoft.Owin.Hosting;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string baseAddress = "http://localhost:8008/";
+
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                Console.WriteLine($"Sample running at {baseAddress}. Press enter to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- src/Sequin.Sample/Properties/AssemblyInfo.cs
+++ src/Sequin.Sample/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b2360f29-2fe9-47f8-a0b9-fb2c1b5f72e4")]
+[assembly: Guid("0ab07941-b3f8-41ff-94c8-cb6e8ca56e71")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
